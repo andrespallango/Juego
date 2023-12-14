555275ba14af0484b57b1bf7f1a6a2b1e1beed1f
andrespallango <avpallango@espe.edu.ec>
2023-12-14 11:42:37 -0500

    Screen

--- app/src/main/java/com/example/juego/MainActivity.java
+++ app/src/main/java/com/example/juego/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.juego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnJugar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnJugar=findViewById(R.id.bt_jugar);
+
+        btnJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,Juego.class);
+                startActivity(intent);
+            }
+        });
     }
 }
