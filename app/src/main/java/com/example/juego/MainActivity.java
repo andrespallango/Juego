3fdb2eacfbdbdaaab7ddf25354c49a0521aeda0e
andrespallango <avpallango@espe.edu.ec>
2023-12-14 12:02:18 -0500

    Botón salir

--- app/src/main/java/com/example/juego/MainActivity.java
+++ app/src/main/java/com/example/juego/MainActivity.java
@@ -8,18 +8,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button btnJugar;
+Button btnJugar, btnSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnJugar=findViewById(R.id.bt_jugar);
+        btnSalir=findViewById(R.id.bt_salir);
 
         btnJugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(MainActivity.this,Juego.class);
                 startActivity(intent);
+            }
+        });
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
